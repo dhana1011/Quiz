d50e47aaec08e71a1794e9ce6b8bd373e888ec8e
dhana1011 <156107968+dhana1011@users.noreply.github.com>
2025-02-06 15:25:07 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,101 @@
+let questions = [
+    {
+        numb: 1,
+        question: "If a:b = 2:3; b:c = 4:5; and d is 20% more than c, find the ratio between a and d?",
+        answer: "A. 4:9",
+        options: [
+            "A. 4:9",
+            "B. 8:9",
+            "C. 4:7",
+            "D. 1:4"
+        ]
+    },
+    {
+        numb: 2,
+        question: "A boat can go 48km upstream and 36km downstream in 5.8 hrs. The speed of the boat in still water is 16km/h. How much time (in hrs) will the boat take to go 54km upstream and 40km downstream?",
+        answer: "B. 6.5",
+        options: [
+            "A. 6.8",
+            "B. 6.5",
+            "C. 7.2",
+            "D. 7.5"
+        ]
+    },
+    {
+        numb: 3,
+        question: "One part of the sentence below may contain an error. Identify the part. If there is no error, choose ‘No error’. When Shailja will come, we will have to talk about the issue together.",
+        answer: "D. When Shailja will come,",
+        options: [
+            "A. No error",
+            "B. about the issue together.",
+            "C. we will have to talk",
+            "D. When Shailja will come,"
+        ]
+    },
+    {
+        numb: 4,
+        question: "One part of the sentence below may contain an error. Identify the part. If there is no error, choose ‘No error’. Real Madrid brilliantly played against Barcelona last night.",
+        answer: "A. brilliantly played",
+        options: [
+            "A. brilliantly played",
+            "B. against Barcelona last night.",
+            "C. Real Madrid",
+            "D. No error."
+        ]
+    },
+    {
+        numb: 5,
+        question: "Let us wait ______ the clock strikes twelve.",
+        answer: "C. until",
+        options: [
+            "A. up to",
+            "B. since",
+            "C. until",
+            "D. unless"
+        ]
+    },
+    {
+        numb: 6,
+        question: "I ___________ waiting for two hours and I cannot wait any longer.",
+        answer: "A. have been",
+        options: [
+            "A. have been",
+            "B. would been",
+            "C. would have been",
+            "D. should be"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Select the most appropriate option to fill in the blanks. Srikant _______ his corporate job and joined the TASC force to ____ his country.",
+        answer: "C. relinquished; serve",
+        options: [
+            "A. relinquish; serve",
+            "B. has relinquish; served",
+            "C. relinquished; serve",
+            "D. relinquished: served"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Which is the WRONG term in the following series? bfj, ddl, gao, iyq, lwt, ntv",
+        answer: "C. gao",
+        options: [
+            "A. ntv",
+            "B. lwt",
+            "C. gao",
+            "D. iyq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "P is the father of Q and brother of R whose son is the brother of T. U is the mother of Q and daughter-in-law of V. Z is the mother of R. W is the mother of T and is married to R. How is V related to T?",
+        answer: "B. Paternal grandfather",
+        options: [
+            "A. Paternal grandmother",
+            "B. Paternal grandfather",
+            "C. Maternal grandfather",
+            "D. Uncle"
+        ]
+    }
+];
